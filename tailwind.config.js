94f59dfc802f1793b1e3e691580e6a667fc5e2b9
Mike Zhang <106291455+mikexd-dev@users.noreply.github.com>
2023-04-12 15:40:46 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        "bounce-once": "bounce 2s ease-in-out 1",
+      },
+    },
+  },
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/forms"),
+    require("tailwindcss-debug-screens"),
+  ],
+  // daisyUI config
+  daisyui: {
+    styled: true,
+    themes: ["lofi", "business"],
+    base: true,
+    utils: true,
+    logs: false,
+    rtl: false,
+    darkTheme: "business",
+  },
+};
